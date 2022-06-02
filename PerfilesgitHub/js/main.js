d4405bb06560e561e90855b7ecf985fd97260d1a
Haymer28 <ghaymer24@gmail.com>
2022-06-02 18:25:34 -0500

    Demasiados cambios

--- PerfilesgitHub/js/main.js
+++ PerfilesgitHub/js/main.js
@@ -7,18 +7,62 @@ const app = Vue.createApp({
         return {
           message: 'Bienvenido!',
           busqueda: null,
-          result: null
+          result: null,
+          error: null,
+          favoritos: new Map() 
+        }
+      },
+      created(){
+        const FavoritoGuardados = JSON.parse(window.localStorage.getItem("misfavoritos"))
+        if(FavoritoGuardados.lenght){
+
+          //const favoritosnew = 
+
         }
       },
     //La palabra de function ya no es necesario ya que se usa en motodo
-    methods: {
-      async Buscar(){
-        const response = await fetch(API + this.busqueda)
-        const data = await response.json()
-        console.log(data)
-        this.result = true
-    }
-    },
-})   
-Buscar();                                           
+    computed:{
+      //Queremos saber si el usuario esta en favorito
+      estaFavoritos(){
+      return this.favoritos.has(this.result.id)
+      },
 
+      TodosFavoritos(){
+        return Array.from(this.favoritos.values())
+        //El metodo value trera todos los valores sin las claves
+      
+      }
+    },
+
+    methods: {
+      async buscar() {
+          //depende del error
+          this.result = this.error = null
+          try {
+              const response = await fetch(API + this.busqueda)
+              if (!response.ok) throw new Error("Usuario no encontrado")
+              //console.log(response)
+              //ahora quiero traer la info en formato json
+              const data = await response.json()
+              console.log(data)
+              this.result = data //cambiar true por data
+          } catch (error) {
+              this.error = error
+              //tan pronto termina el proceso, limpia haciendo vacia la busqueda
+          } finally {
+              this.busqueda = null
+          }
+      }, //
+            addFavorito(){
+              this.favoritos.set(this.result.id, this.result)
+              this.actualizarEstorage()
+      },
+            removeFavorito(){
+            this.favoritos.delete(this.result.id)
+            this.actualizarEstorage()
+      },
+            actualizarEstorage(){
+              window.localStorage.setItem('misfavoritos', JSON.stringify(this.TodosFavoritos))
+            }
+  }
+})
